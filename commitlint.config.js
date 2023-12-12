390074e8ca413c5318ff69547f0cde6b01509abe
Paulo Sergio <pauloserg861@gmail.com>
2023-12-11 23:45:21 -0300

    chore: remove subject from commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,7 @@
-module.exports = { 
+module.exports = {
     extends: ["@commitlint/config-conventional"],
     rules: {
         'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'perf']],
-        'subject-case': [2, 'always', 'sentence-case'],
         // Add or customize rules based on your project's needs
-      } 
+      }
 };
